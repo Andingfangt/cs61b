cb1a0013ee3daaf0572579dd9fb793bec7535ace
mmh <miaominghao@qq.com>
2023-08-23 11:47:27 +0800

    add st master

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,11 +5,11 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n%2 == 0) {
-            return n/2;
+        if (n % 2 == 0) {
+            return n / 2;
         } 
         else {
-            return n*3 + 1;
+            return n * 3 + 1;
         }
     }
 
